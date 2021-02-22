b1446b133c31794882a2ca28e50b373c1b1cf9ec
Todd Blackman <tdblackman@gmail.com>
2021-02-22 11:19:25 -0500

    Remove to_h hack that's breaking things as a test

--- lib/composite_primary_keys/relation/where_clause.rb
+++ lib/composite_primary_keys/relation/where_clause.rb
@@ -1,24 +1,26 @@
 module ActiveRecord
   class Relation
     class WhereClause
-      def to_h(table_name = nil)
-        equalities = equalities(predicates)
+      # Test removing this hack
+      #
+      # def to_h(table_name = nil, equality_only: false)
+      #   equalities = equalities(predicates)
 
-        # CPK Adds this line, because ours are coming in with AND->{EQUALITY, EQUALITY}
-        equalities = predicates.grep(Arel::Nodes::And).map(&:children).flatten.grep(Arel::Nodes::Equality) if equalities.empty?
+      #   # CPK Adds this line, because ours are coming in with AND->{EQUALITY, EQUALITY}
+      #   equalities = predicates.grep(Arel::Nodes::And).map(&:children).flatten.grep(Arel::Nodes::Equality) if equalities.empty?
 
-        if table_name
-          equalities = equalities.select do |node|
-            node.left.relation.name == table_name
-          end
-        end
+      #   if table_name
+      #     equalities = equalities.select do |node|
+      #       node.left.relation.name == table_name
+      #     end
+      #   end
 
-        equalities.map { |node|
-          name = node.left.name.to_s
-          value = extract_node_value(node.right)
-          [name, value]
-        }.to_h
-      end
+      #   equalities.map { |node|
+      #     name = node.left.name.to_s
+      #     value = extract_node_value(node.right)
+      #     [name, value]
+      #   }.to_h
+      # end
     end
   end
 end
